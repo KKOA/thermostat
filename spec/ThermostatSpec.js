ef46075b0c8b7b899a37486e6e77247cfbadb0dd
keith(mac) <ka33145@googlemail.com>
2017-09-27 21:10:50 +0100

    Fixed bug with lowerTemp

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -19,18 +19,29 @@ describe("Thermostat", function () {
   });
   describe("lowerTemp", function () {
     it("reduce temperature by 1", function () {
-
       myThermostat.lowerTemp();
       expect(myThermostat.getTemp()).toEqual(DEFAULT_TEMP - 1);
     });
-  });
-  describe("getPowerStatus", function () {
-    it("return power saving mode", function () {
-      expect(myThermostat.getPowerStatus()).toEqual('on');
+
+    it("prevent reduce temperature ( "+MIN_TEMP+" )", function () {
+      for (var i = 1 ; i < 20 ; i++ )
+      {
+          console.log(myThermostat.isMinTemp())
+          myThermostat.lowerTemp();
+          console.log(myThermostat.getTemp());
+      }
+      expect(myThermostat.getTemp()).toEqual(MIN_TEMP);
     });
   });
+
+  describe("getPSM", function () {
+    it("return true by default", function () {
+      expect(myThermostat.getPSM()).toEqual(true);
+    });
+  });
+
   describe("resetTemp", function () {
-    it("set tempeature back to default (" + DEFAULT_TEMP + ")", function () {
+    it("set tempeature back to default ( " + DEFAULT_TEMP + " )", function () {
       myThermostat.resetTemp();
       expect(myThermostat.getTemp()).toEqual(DEFAULT_TEMP);
     });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,27 +1,37 @@
+/*jshint esversion: 6 */
+
 
 const DEFAULT_TEMP = 20;
+const MIN_TEMP = 10;
 
 function Thermostat() {
   this.temp = DEFAULT_TEMP;
-  this.psm = 'on'
+  this.psm = true;
 }
 
 Thermostat.prototype.getTemp = function() {
   return this.temp;
-}
+};
 
 Thermostat.prototype.raiseTemp = function() {
   this.temp +=  1;
-}
+};
+
+Thermostat.prototype.isMinTemp = function() {
+  return this.temp === MIN_TEMP;
+};
 
 Thermostat.prototype.lowerTemp = function() {
-  this.temp -= 1;
-}
-
-Thermostat.prototype.getPowerStatus = function() {
-  return this.psm;
-}
+  if(this.isMinTemp()=== false)
+  {
+    this.temp -= 1;
+  }
+};
 
 Thermostat.prototype.resetTemp = function(number) {
   this.temp = DEFAULT_TEMP;
-}
+};
+
+Thermostat.prototype.getPSM = function() {
+  return this.psm;
+};
